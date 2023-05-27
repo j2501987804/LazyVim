29edb3c1a18f1b8b961cb102987d2e2501892f8f
Crazycat <41771911+j2501987804@users.noreply.github.com>
2023-05-27 10:47:03 +0800

    Update core.lua

--- lua/lazyvim/plugins/core.lua
+++ lua/lazyvim/plugins/core.lua
@@ -2,5 +2,5 @@ require("lazyvim.config").init()
 
 return {
   { "folke/lazy.nvim", version = "*" },
-  { "LazyVim/LazyVim", priority = 10000, lazy = false, config = true, cond = true, version = "*" },
+  { "j2501987804/LazyVim", priority = 10000, lazy = false, config = true, cond = true, version = "*" },
 }
